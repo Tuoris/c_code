8ba65dc3f8308d358cf65500ae691affee0d7fca
Tuoris <yuriy.tupychak@gmail.com>
2016-01-10 14:47:27 +0200

    add checking for palindome

--- string_reverse.c
+++ string_reverse.c
@@ -1,6 +1,10 @@
 #include "stdio.h"
 #define MAX_LEN 80
 
+void reverse(char * string, char * reversed);
+void insant_reverse(char * string);
+int ispalindrome(char * string);
+
 // Didn't want use string.h
 int len(char * string) {
     int len = 0;
@@ -8,9 +12,30 @@ int len(char * string) {
     return len;
 }
 
+// Didn't want use ctype.h
+int tolower(int c) {
+    if (c < 'A' || c > 'Z')
+        return c;
+    else
+        return c - 'A' + 'a';
+}
+
+int main(void) {
+    char string [MAX_LEN];
+    printf("Enter any string\n>>> ");
+    fgets(string, MAX_LEN, stdin);
+    char reversed [MAX_LEN];
+    reverse(string, reversed);
+    printf("Entered string: %s\n", string);
+    printf("Reversed string: %s\n", reversed);
+    insant_reverse(string);
+    printf("Instant reverse: %s\n", string);
+    printf("Palindrome: %s\n", ispalindrome(string)? "yes": "no");
+    return 0;
+}
+
 void reverse(char * string, char * reversed) {
     int slen = len(string);
-    printf("%d\n", slen);
     for (int index = 0; index < slen; index++) {
         reversed[slen - index - 1] = string[index];
     }
@@ -28,15 +53,14 @@ void insant_reverse(char * string) {
     }
 }
 
-int main(void) {
-    char string [MAX_LEN];
-    printf("Enter any string\n>>> ");
-    fgets(string, MAX_LEN, stdin);
-    char reversed [MAX_LEN];
-    reverse(string, reversed);
-    printf("Entered string: %s\n", string);
-    printf("Reversed string: %s\n", reversed);
-    insant_reverse(string);
-    printf("Instant reverse: %s\n", string);
-    return 0;
+int ispalindrome(char * string) {
+    int p_flag = 1;
+    int slen = len(string);
+    for (int index = 0; index < slen; index++) {
+        if (tolower(string[index]) != tolower(string[slen - index - 1])) {
+            p_flag = 0;
+            break;
+        }
+    }
+    return p_flag;
 }
